225b68f284f6e912224f323905f0854c8c24ee19
Sine Jespersen <sinejespersen@Sines-MacBook-Pro.local>
2018-05-02 14:44:29 +0200

    now it calculates wpm with breaks between sentences

--- src/logic.js
+++ src/logic.js
@@ -45,13 +45,6 @@ $('#startover').on('click', function() {
   startOver(howmany);
 });
 
-$('#finish').on('click', function() {
-  let wpmString = wpm();
-  $('#result').html('wmp: ' + wpmString + '<br>' + finishedString);
-  done = true;
-  $('#input').prop('disabled', true);
-});
-
 let resultString = '';
 let arrayOfInputs = [];
 let finishedString = '';
@@ -80,54 +73,71 @@ $(function() {
     } else if (e.key.toLowerCase() == 'enter') {
       commenceDoneProcedure(resultString);
       resultString = '';
+    } else if (e.key.toLowerCase() == 'escape') {
+      doneProcedure();
+      return;
     }
     arrayOfInputs.push(e.timeStamp);
   });
 });
 
+function doneProcedure() {
+  let wpmString = wpm();
+  $('#result').html('wmp: ' + wpmString + '<br>' + finishedString);
+  done = true;
+  $('#input').prop('disabled', true);
+  startOver();
+}
+
+let time = 0;
 let howLongTyped = 0;
 let words = 0;
 function commenceDoneProcedure(result) {
-  // let firstTimeStamp = arrayOfInputs[0];
-  // let lastTimeStamp = arrayOfInputs[arrayOfInputs.length - 1];
-  // console.log(firstTimeStamp);
-  // console.log(lastTimeStamp);
-  // let howLongTyped = lastTimeStamp - firstTimeStamp;
-  // console.log(howLongTyped);
   howLongTyped = $('#time').html();
+  $('#time').html('');
+  min = 0;
+  sec = 0;
   let stringFromInput = $('#input').val();
   let areThereSpecialCharacters = stringFromInput !== result;
 
   words += parseInt(result.trim().split(/\s+/).length);
 
   finishedString += `
-    [skrevet: ${result}, rettet: ${stringFromInput}, forventet: ${currentString}, tid: ${Math.round(
-    howLongTyped
-  )}, corrected text: ${areThereSpecialCharacters}] <br>`;
+    [skrevet: ${result}, rettet: ${stringFromInput}, forventet: ${currentString}, tid: ${howLongTyped}, corrected text: ${areThereSpecialCharacters}] <br>`;
 
   changeSentence();
   $('#input').val('');
   if (done) {
-    $('#finish').click();
+    doneProcedure();
   }
 }
 
 function wpm() {
-  return words / (min + sec / 60);
+  return words / (sumMin + sumSec / 60);
 }
 
+let sumSec = 0;
+let sumMin = 0;
 let sec = 0;
 let min = 0;
 function startClock() {
   timestarted = true;
   var handler = function() {
     if (!done) {
+      sumSec++;
       if (++sec === 60) {
+        sumMin++;
         sec = 0;
+        sumSec = 0;
         if (++min === 60) min = 0;
       }
       $('#time').html(
         (min < 10 ? '0' + min : min) + ':' + (sec < 10 ? '0' + sec : sec)
+      );
+      $('#sumtime').html(
+        (sumMin < 10 ? '0' + sumMin : sumMin) +
+          ':' +
+          (sumSec < 10 ? '0' + sumSec : sumSec)
       );
       $('#words').html(words);
     }
