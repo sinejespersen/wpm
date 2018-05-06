9128a4ef35292fdf5f621e131cf053508e2fd1fe
Sine Jespersen <sinejespersen@Sines-MacBook-Pro.local>
2018-05-06 10:20:03 +0200

    works in firefox

--- src/logic.js
+++ src/logic.js
@@ -1,5 +1,21 @@
 import _ from 'underscore';
-var proverbs;
+let proverbs;
+let done = false;
+let counter = 0;
+let currentString = '';
+let resultString = '';
+let arrayOfInputs = [];
+let finishedString = '';
+let timestarted = false;
+let time = 0;
+let howLongTyped = 0;
+let words = 0;
+let sumSec = 0;
+let sumMin = 0;
+let sec = 0;
+let min = 0;
+let refreshIntervalId = 0;
+
 $.ajax({
   url: 'proverbs.txt',
   dataType: 'text',
@@ -21,14 +37,10 @@ function startOver(howmany) {
   });
 }
 
-let done = false;
-let counter = 0;
-
 function changeSentence() {
   $('#sentence').html(getSentence());
 }
 
-let currentString = '';
 function getSentence() {
   if (proverbs.length > 0) {
     currentString = _.sample(proverbs);
@@ -45,17 +57,13 @@ $('#startover').on('click', function() {
   startOver(howmany);
 });
 
-let resultString = '';
-let arrayOfInputs = [];
-let finishedString = '';
-let timestarted = false;
-
 $(function() {
   if (done) {
     return;
   }
   $('#input').keydown(function(e) {
     if (!timestarted) {
+      console.log('sdfsdf');
       startClock();
     }
     if (e.key.length == 1) {
@@ -71,28 +79,32 @@ $(function() {
     } else if (e.key.toLowerCase() == 'arrowdown') {
       resultString += '↓';
     } else if (e.key.toLowerCase() == 'enter') {
-      commenceDoneProcedure(resultString);
+      enterHasBeenPressed(resultString);
+      $('#time').html('00:00');
+      $('#words').html(words);
       resultString = '';
+      clearInterval(refreshIntervalId);
+      timestarted = false;
     } else if (e.key.toLowerCase() == 'escape') {
-      doneProcedure();
+      finishedProcedure();
       return;
     }
     arrayOfInputs.push(e.timeStamp);
   });
 });
 
-function doneProcedure() {
-  let wpmString = wpm();
-  $('#result').html('wmp: ' + wpmString + '<br>' + finishedString);
+function finishedProcedure() {
+  let wpmString = `wpm is ${wpm()}`;
+  finishedString = `${wpmString} \n ${finishedString}`;
+  $('#result').html(finishedString.replace(/(\r\n|\n|\r)/gm, '<br>'));
+  finishedString = encodeURIComponent(finishedString);
   done = true;
-  $('#input').prop('disabled', true);
-  startOver();
+  window.open(
+    `mailto:sinejespersen@gmail.com?subject=WPM&body=${finishedString}`
+  );
 }
 
-let time = 0;
-let howLongTyped = 0;
-let words = 0;
-function commenceDoneProcedure(result) {
+function enterHasBeenPressed(result) {
   howLongTyped = $('#time').html();
   $('#time').html('');
   min = 0;
@@ -103,12 +115,12 @@ function commenceDoneProcedure(result) {
   words += parseInt(result.trim().split(/\s+/).length);
 
   finishedString += `
-    [skrevet: ${result}, rettet: ${stringFromInput}, forventet: ${currentString}, tid: ${howLongTyped}, corrected text: ${areThereSpecialCharacters}] <br>`;
+    ${result}, ${stringFromInput}, ${currentString}, ${howLongTyped}, ${areThereSpecialCharacters}, \n`;
 
   changeSentence();
   $('#input').val('');
   if (done) {
-    doneProcedure();
+    finishedProcedure();
   }
 }
 
@@ -116,14 +128,10 @@ function wpm() {
   return words / (sumMin + sumSec / 60);
 }
 
-let sumSec = 0;
-let sumMin = 0;
-let sec = 0;
-let min = 0;
 function startClock() {
   timestarted = true;
   var handler = function() {
-    if (!done) {
+    if (!done && timestarted) {
       sumSec++;
       if (++sec === 60) {
         sumMin++;
@@ -139,9 +147,9 @@ function startClock() {
           ':' +
           (sumSec < 10 ? '0' + sumSec : sumSec)
       );
-      $('#words').html(words);
     }
   };
-  setInterval(handler, 1000);
+
+  refreshIntervalId = setInterval(handler, 1000);
   handler();
 }
